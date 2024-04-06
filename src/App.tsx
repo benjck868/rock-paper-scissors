60f62da88cfccc4d1bf6ceccd4bfabd064ebe5f1
Benjamin B. Sumilhig <benjamin.b.sumilhig@gmail.com>
2024-04-06 18:05:44 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home/Home'
 import Main from './layout/main/Main'
+import Index from './pages/index/Index'
 import Advance from './pages/advanced/Advance'
 function App() {
    return (
     <>
       <Routes>
-        <Route element={<Main />}>
-          <Route index path='/' element={<Home />} />
-          <Route path='/advance' element={<Advance />} />
+        <Route path='/' element={<Index />} />
+        <Route path='/game' element={<Main />}>
+          <Route index element={<Home />} />
+          <Route path='advance' element={<Advance />} />
         </Route>
       </Routes>
     </>
--- src/pages/advanced/Advance.tsx
+++ src/pages/advanced/Advance.tsx
@@ -3,10 +3,13 @@ import BeforePickAdvance from "@/components/BeforePickAdvance/BeforePickAdvance"
 import { RootState } from "@/state/store";
 import { useDispatch, useSelector } from "react-redux";
 import {setMode} from '@/state/game/gameSlice'
+import { useEffect } from "react";
 export default function Advance() {
   const duel = useSelector((state: RootState)=>state.game.duel)
   const dispatcher = useDispatch()
-  dispatcher(setMode('advance'))
+  useEffect(()=>{
+    dispatcher(setMode('advance'))
+  })
   return (
     <>
       {!duel?<BeforePickAdvance /> : <AfterPick />}
